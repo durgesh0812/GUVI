9b48db5b10df9db5c7acc05d80581cb4b823c6fa
durgesh0812 <74395030+durgesh0812@users.noreply.github.com>
2020-11-19 21:34:30 +0530

    Add files via upload

---
+++ MINIMUM AMONG 10 NUMBER.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+    var a=userInput[0].split(" ");
+       
+       var b=Math.min(...a)
+    console.log(b)
+;    
+    
+    
+})
